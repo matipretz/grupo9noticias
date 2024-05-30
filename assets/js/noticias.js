eb4f4a29a2f27d5321c02001da6297a50c22e8a4
Matias Martin Murad Pretz <30426732+matipretz@users.noreply.github.com>
2024-05-29 22:55:14 -0300

    modified:   assets/js/noticias.js

--- assets/js/noticias.js
+++ assets/js/noticias.js
@@ -1,38 +1,34 @@
-const LeerApi = async () => {
-  let url = "https://grupo9.vercel.app/json/noticias.json";
-  const res = await fetch(url);
-  const datos = await res.json();
-  return datos;
-};
+fetch('https://grupo9back-production.up.railway.app/noticias')
+  .then(res => res.json())
+  .then(noticias => {
+    const html = noticias
+      .map(noticia => {
+        return `
+        <article data-id="${noticia.id}">
+          <div class="unaNotadetalle aparecer">
+            <div class="noticiasfoto">
+              <img src="${noticia.imagenUrl}" alt="${noticia.titulo}">
+            </div>
+            <div class="noticiasTexto">
+              <p class="text-sm font-medium uppercase tracking-widest text-pink-500">${noticia.categoria}</p>
+              <p class="text-xl font-bold  sm:text-2xl slide-in-left"> ${noticia.titulo}</p>
+              <p class="text-sm copete"> ${noticia.copete}</p>
+            </div>
+          </div>
+        </article>
+        `
+      })
+      .join('')
 
-async function Noticias() {
-  const contenedor = document.getElementById("contenedorListadoNoticias");
-  contenedor.innerHTML = '<div align="center">Cargando noticias...</div> ';
+    const contenedor = document.getElementById('contenedorListadoNoticias')
+    contenedor.innerHTML = html
+  })
+  .catch(error => {
+    console.error('Error al cargar las noticias:', error)
+    const contenedor = document.getElementById('contenedorListadoNoticias')
+    contenedor.innerHTML =
+      '<div align="center">Error al cargar las noticias.</div>'
+  })
 
-  console.log(" cargando noticias ");
-  await LeerApi()
-    .then((resultadoapi) => {
-      contenedor.innerHTML = "";
-
-      for (let dato of resultadoapi.data) {
-        contenedor.innerHTML +=
-          '<div class="unaNotadetalle aparecer">' +
-          '<div class="noticiasfoto"><img alt="" src="' +
-          dato.imagenUrl +
-          '"></div>' +
-          '<div class="noticiasTexto">' +
-          '<p class="text-sm font-medium uppercase tracking-widest text-pink-500">' +
-          dato.categoria +
-          " </p>" +
-          '<p class="text-xl font-bold  sm:text-2xl slide-in-left">' +
-          dato.titulo +
-          "</p>" +
-          '<p class="text-sm copete">' +
-          dato.copete +
-          " </p></div></div>";
-      }
-    })
-    .catch((err) => (contenedor.innerHTML = "Ups Error<br>"));
-}
-
-Noticias();
+const contenedor = document.getElementById('contenedorListadoNoticias')
+contenedor.innerHTML = '<div align="center">Cargando noticias...</div>'
